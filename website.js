3a23f66f512b41b154afb44cce33c9f2700b1983
Ina Patrice <inapatricegonzales@gmail.com>
2021-01-20 16:17:38 -0500

    date and time updates

--- website.js
+++ website.js
@@ -1,10 +1,32 @@
+// Ina Patrice
+// website.js
 // JavaScript file for website inapatrice.github.io
 
-var today = new Date();
+// date and time at the beginning of the page
+var headerClock = setInterval(dateClock, 0);
 
-const months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
-var month = months[today.getMonth()]
+// prints the date and time on the screen at the beginning of the page
+function dateClock() {
+	var today = new Date();
+	// date
+	const months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	var month = months[today.getMonth()]
+	var date = month + ' ' + today.getDate() + ' ' + today.getFullYear() + ' at ';
+	// local time for users
+	var time = today.toLocaleTimeString();
+	// print to screen
+	document.getElementById("date").innerHTML = "Current Login: " + date + time;
+}
 
-var date = month + ' ' + today.getDate() + ' ' + today.getFullYear() + ' at ';
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + " (EST)";
-document.getElementById("date").innerHTML = "Current Login: " + date + time;
+// blinking cursor
+        var cursor = true;
+        var speed = 250;
+        setInterval(() => {
+          if(cursor) {
+            document.getElementById('cursor').style.opacity = 0;
+            cursor = false;
+          }else {
+            document.getElementById('cursor').style.opacity = 1;
+            cursor = true;
+          }
+        }, speed);
